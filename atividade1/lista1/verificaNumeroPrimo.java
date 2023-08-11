7dc40e74f1577261630d0b43c3dbc33036c634d3
Joel-ASantos <joel.alves.santos77@gmail.com>
2023-08-11 10:39:39 -0300

    Adicionando no Github

---
+++ atividade1/lista1/verificaNumeroPrimo.java
@@ -0,0 +1,13 @@
+package atividade1.lista1;
+import java.util.Scanner;
+
+//Crie um algoritmo para verificar numeros primos
+public class verificaNumeroPrimo{
+    public static void main(String args[]) {
+        verificaNumeroPrimo(n:12324354545);
+    }
+
+    public static void verificarNumeroPrimo(Integer n){
+        
+    }
+}
