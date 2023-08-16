cb21b7469092975a0741ee088a672416838d802b
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-08-15 23:55:12 -0300

    Atividade de verificar numeros primos concluida

--- atividade1/lista1/verificaNumeroPrimo.java
+++ atividade1/lista1/verificaNumeroPrimo.java
@@ -1,13 +1,32 @@
 package atividade1.lista1;
 import java.util.Scanner;
+import java.lang.Math;
 
-//Crie um algoritmo para verificar numeros primos
 public class verificaNumeroPrimo{
-    public static void main(String args[]) {
-        verificaNumeroPrimo(n:12324354545);
+    public static void main(String args[]){
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("Digite o numero para verificao: ");
+        Integer numero = Integer.parseInt(num.nextLine());
+
+        if(verificarNumeroPrimo(numero) >= 1){
+            System.out.println("\nEh primo");
+        }else{
+            System.out.println("\nNao eh primo");
+        }
     }
 
-    public static void verificarNumeroPrimo(Integer n){
-        
+    public static Integer verificarNumeroPrimo(Integer n){
+        if(n % 2 == 0){
+            return 0;
+        }
+
+        for(Integer i = 2;i <= Math.sqrt(n);i++){
+            if(n % i == 0){
+                return 0;
+            }
+        }
+
+        return 1;
     }
 }
