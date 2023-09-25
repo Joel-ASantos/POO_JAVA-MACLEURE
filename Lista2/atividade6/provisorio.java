efbda179ddea1824c6820f180d8d7b8c844d3c14
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-09-25 16:55:16 -0300

    add

---
+++ Lista2/atividade6/provisorio.java
@@ -0,0 +1,17 @@
+package Lista2.atividade6;
+
+/**
+ * @brief
+ * 
+ * Criar um algortimo usando os conceitos de
+ * herança e encapsulamento. Ele deve ser simples
+ * contendo 4 classes.
+ * 
+ * OBS: Esse arquivo é provisório.
+ */
+
+public class provisorio {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
