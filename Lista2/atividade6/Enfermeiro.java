5fa80bb9f720fd7b1242934aed31920d0501bdff
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-09-27 10:34:05 -0300

    add

---
+++ Lista2/atividade6/Enfermeiro.java
@@ -0,0 +1,31 @@
+package Lista2.atividade6;
+
+public class Enfermeiro {
+    String coren;
+    float salario;
+    String departamento;
+
+    public String getCoren() {
+        return coren;
+    }
+
+    public void setCoren(String coren) {
+        this.coren = coren;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+}
