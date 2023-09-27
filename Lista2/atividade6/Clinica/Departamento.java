77f45368bfdf7016b23050ac89a2bba91d7360ce
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-09-27 11:00:23 -0300

    classes feitas

--- Lista2/atividade6/Clinica/Departamento.java
+++ Lista2/atividade6/Clinica/Departamento.java
@@ -1,14 +1,21 @@
-package Clinica;
+package Lista2.atividade6.Clinica;
 
-    public class Departamento {
+public class Departamento {
     String identificador,nome;
-}
+
+    public Departamento() {
+    }
+
+    public Departamento(String identificador, String nome) {
+        this.identificador = identificador;
+        this.nome = nome;
+    }
 
     public String getIdentificador() {
         return identificador;
     }
 
-    public void setIdentificador(String) {
+    public void setIdentificador(String identificador) {
         this.identificador = identificador;
     }
 
@@ -20,3 +27,4 @@ package Clinica;
     public void setNome(String nome) {
         this.nome = nome;
     }
+}
