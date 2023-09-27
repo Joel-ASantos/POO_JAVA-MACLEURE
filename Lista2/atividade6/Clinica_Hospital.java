d0a393965c7594818ed7f048ac3dc515a87b0e98
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-09-27 19:07:39 -0300

    Quase lá

--- Lista2/atividade6/Clinica_Hospital.java
+++ Lista2/atividade6/Clinica_Hospital.java
@@ -6,12 +6,24 @@ package Lista2.atividade6;
  * Criar um algortimo usando os conceitos de
  * herança e encapsulamento. Ele deve ser simples
  * contendo 4 classes.
- * 
- * OBS: Esse arquivo é provisório.
  */
 
-public class Clinica_Hospital {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+import Lista2.atividade6.Clinica.Pessoa;
+import Lista2.atividade6.Clinica.Empregado;
+import Lista2.atividade6.Clinica.Enfermeiro;
+import Lista2.atividade6.Clinica.medico;
+import Lista2.atividade6.Clinica.paciente;
+
+public class Clinica_Hospital{
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)throws Exception { 
+       Enfermeiro enfer1 = new Enfermeiro("Julia", "9821321", "29381297", 45, "Emergencia", "C921J", 10000);
+       medico med1 = new medico("Marcos", "890802", "2391879", 36, "Cirurgião", "HB20", 30000);
+       paciente doente = new paciente("Jairo", "2917323", "1500", 20, "NIGG@", "none", "Vasco");
+
+       System.out.println(enfer1);
+       System.out.println(med1);
+       System.out.println(doente);
     }
 }
