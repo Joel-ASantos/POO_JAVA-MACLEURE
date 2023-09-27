b65534209c8d0a59aef0b13b0c0d7ae8959ad84d
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-09-27 10:29:10 -0300

    addPessoa

---
+++ Lista2/atividade6/Pessoa.java
@@ -0,0 +1,38 @@
+package Lista2.atividade6;
+
+public class Pessoa {
+    String nome, cpf,telefone;
+    Integer idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+}
