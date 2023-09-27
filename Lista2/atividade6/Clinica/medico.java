5e60a4197593b7c8d1290dbbe56cce2070e6c718
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-09-27 10:49:36 -0300

    add

--- Lista2/atividade6/Clinica/medico.java
+++ Lista2/atividade6/Clinica/medico.java
@@ -1,5 +1,30 @@
 package Lista2.atividade6.Clinica;
 
 public class medico {
-        
+    String Crm;
+    float Salario;
+
+    public medico() {
+    }
+
+    public medico(String Crm, float Salario) {
+        this.Crm = Crm;
+        this.Salario = Salario;
+    }
+
+    public String getCrm() {
+        return this.Crm;
+    }
+
+    public void setCrm(String Crm) {
+        this.Crm = Crm;
+    }
+
+    public float getSalario() {
+        return this.Salario;
+    }
+
+    public void setSalario(float Salario) {
+        this.Salario = Salario;
+    }
 }
