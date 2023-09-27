5617c46a955cdc325dd7bf729cb51424c244f65a
Ítalo Eugênio <italoeugenio539@gmail.com>
2023-09-27 10:41:26 -0300

    Departamento

---
+++ Lista2/atividade6/Clinica/Departamento.java
@@ -0,0 +1,22 @@
+package Clinica;
+
+    public class Departamento {
+    String identificador,nome;
+}
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String) {
+        this.identificador = identificador;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
