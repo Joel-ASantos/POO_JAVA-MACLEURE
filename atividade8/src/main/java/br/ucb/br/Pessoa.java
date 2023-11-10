873cc4aae56f79638b6cbe8cc545755eb804378e
Joel-ASantos <joel.alves.santos77@gmail.com>
2023-11-10 10:43:29 -0300

    maven-test

---
+++ atividade8/src/main/java/br/ucb/br/Pessoa.java
@@ -0,0 +1,7 @@
+package br.ucb.br;
+
+public class Pessoa{
+    public String dizerOla(){
+        return "Olá, maven!";
+    }
+}
---
+++ atividade8/src/test/java/br/ucb/br/PessoaTest.java
@@ -0,0 +1,18 @@
+package br.ucb.br;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PessoaTest{
+    Pessoa pessoa;
+
+    @BeforeEach
+    public void setUp(){
+        this.pessoa = new Pessoa();
+    }
+
+    @Test
+    public void dizerOlaTest(){
+       assertEquals("Olá, maven!",pessoa.dizerOla());  
+    }
+}
